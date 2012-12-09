5ee337c126143048e3dd6e1b3c31310b9ca0b968
Jacob Williams <gospelbass@gmail.com>
2012-12-09 03:00:15 +0000

    add test

---
+++ test/manual.js
@@ -0,0 +1,22 @@
+var ide = require( 'crosstalk-ide' )();
+var config = require( '../config.json' );
+
+var worker = ide.run( __dirname + "/../index.js", { name : "worker", config : config } );
+var callback = function (error, response) {
+  console.log(error, response);
+}
+var data = { 
+  // to: "gospelbass@gmail.com",
+  from: "jwilliams@fahrenheitmarketing.com",
+  subject: "test message",
+  text: "this is a test message"
+}
+
+worker.send(
+  "sendgrid.smtp.send",
+  data,
+  null,
+  callback
+);
+
+worker.shouldCallCallback(callback);
