4b7b41f9f5ea1b1f1337776eae67c63b068769c3
Jacob Williams <gospelbass@gmail.com>
2012-12-09 02:26:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+sendgrid-smtp-api-worker
+============================
+
+`crosstalk-worker-api-sendgrid-smtp` is a Crosstalk worker for sending emails through the sendgrid rest api
+
+## Configuration
+
+```json
+{
+  "api_user" : "your sendgrid user name",
+  "api_key" : "your sendgrid password
+}
+```
+
+## crosstalk.on
+
+`sendgrid.smtp.send`
+
+## crosstalk.emit
+
+None
+
+## http in
+
+None
+
+## http out
+
+None
+
+## https in
+
+None
+
+## https out
+
+`sendgrid.com` - For communicating with Sendgrid API
+
---
+++ index.js
@@ -0,0 +1,89 @@
+/*
+ * index.js: Sendgrid SMTP API Worker
+ *
+ * (C) 2012 Jacob Williams.
+ */
+"use strict";
+
+var async = require( 'async' ),
+    https = require( 'https' ),
+    querystring = require( 'querystring' ),
+    _ = require( 'underscore' ),
+    config = require ( 'config' );
+
+var API_ENDPOINT = "sendgrid.com",
+    HTTP_VERB = "POST",
+    REQUEST_URI = "/api/mail.send.json";
+
+var send = function send( params, callback ) {
+
+  callback = callback || function() { };
+  params = params || {};
+
+  //
+  // required params
+  //
+  if ( ! params.to ) return callback( { message : "missing recipient" } );
+  if ( ! params.from ) return callback( { message : "missing sender" } );
+  if ( ! params.subject ) return callback( { message : "missing subject" } );
+  if ( ! params.text && ! params.html ) return callback( { message : "missing body" } );
+
+  if ( params.toname && _.isArray(params.to)
+      && ( ! _.isArray(params.toname) || params.toname.length !== params.to.length) ) {
+    return callback( { message: "tonames length does not match to length" } );
+  }
+
+  var data = {
+    api_user : config.api_user,
+    api_key : config.api_key,
+    to : params.to,
+    from : params.from,
+    subject : params.subject,
+    text : params.text,
+    html : params.html,
+    bcc : params.bcc,
+    replyTo : params.replyTo
+  }
+
+  var postData = querystring.stringify(data);
+
+  var requestOptions = {
+    host : API_ENDPOINT,
+    method : HTTP_VERB,
+    path : REQUEST_URI,
+    headers : {
+      'Content-Type' : 'application/x-www-form-urlencoded',
+      'Content-Length' : postData.length
+    }
+  }
+
+  var req = https.request( requestOptions );
+
+  req.on( 'response', function ( response ) {
+
+    var body = "";
+
+    response.setEncoding( 'utf8' );
+    response.on( 'data', function ( chunk ) {
+      body += chunk;
+    });
+
+    response.on( 'end', function () {
+      var json = JSON.parse(body);
+      return json.message == "success" ?
+        callback( null, json ) : callback( json.error );
+    });
+
+  }); // req.on 'response'
+
+  req.on( 'error', function ( error ) {
+    return callback( error );
+  });
+
+  req.write(postData);
+
+  req.end();
+
+}; // makeRequest
+
+crosstalk.on( 'sendgrid.smtp.send', send );
